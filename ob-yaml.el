ac90bc5713dcc265b99dcab384d3cdc048d86272
Ladislav Lhotka <ladislav.lhotka@nic.cz>
2023-02-10 13:29:24 +0100

    Remove/adapt comments with template instructions

--- ob-yaml.el
+++ ob-yaml.el
@@ -49,17 +49,15 @@
 (require 'ob-comint)
 (require 'ob-eval)
 
-;; optionally define a file extension for this language
+;; define a file extension for YAML
 (add-to-list 'org-babel-tangle-lang-exts '("yaml" . "yaml"))
 
-;; optionally declare default header arguments for this language
+;; built-in default header arguments are fine
 (defvar org-babel-default-header-args:yaml '())
 
-;; This function expands the body of a source code block by doing things like
-;; prepending argument definitions to the body, it should be called by the
-;; `org-babel-execute:yaml' function below. Variables get concatenated in
-;; the `mapconcat' form, therefore to change the formatting you can edit the
-;; `format' form.
+;; The remaining functions are left unimplemented as we don't need
+;; any evaluation of YAML blocks.
+
 (defun org-babel-expand-body:yaml (body params &optional processed-params)
   "Expand BODY according to PARAMS, return the expanded body."
   (require 'inf-yaml nil t)
@@ -72,25 +70,6 @@
       vars "\n")
      "\n" body "\n")))
 
-;; This is the main function which is called to evaluate a code
-;; block.
-;;
-;; This function will evaluate the body of the source code and
-;; return the results as emacs-lisp depending on the value of the
-;; :results header argument
-;; - output means that the output to STDOUT will be captured and
-;;   returned
-;; - value means that the value of the last statement in the
-;;   source code block will be returned
-;;
-;; The most common first step in this function is the expansion of the
-;; PARAMS argument using `org-babel-process-params'.
-;;
-;; Please feel free to not implement options which aren't appropriate
-;; for your language (e.g. not all languages support interactive
-;; "session" evaluation).  Also you are free to define any new header
-;; arguments which you feel may be useful -- all header arguments
-;; specified by the user will be available in the PARAMS variable.
 (defun org-babel-execute:yaml (body params)
   "Execute a block of YAML code with org-babel.
 This function is called by `org-babel-execute-src-block'"
@@ -125,8 +104,6 @@ This function is called by `org-babel-execute-src-block'"
     ;; function is used in the language files)
     ))
 
-;; This function should be used to assign any variables in params in
-;; the context of the session environment.
 (defun org-babel-prep-session:yaml (session params)
   "Prepare SESSION according to the header arguments specified in PARAMS."
   )
